a4b933626a5cadd09328f1415ae8ef64e47f338e
«Clash-yabi» <yabesolo@student.arteveldehs.be>
2025-05-18 14:27:16 +0200

    add basic style to backlog and nav buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PaginatedBacklog from "./components/PaginatedBacklog.jsx";
+import "./styles/App.css";
+import "./styles/Pagination.css";
 
 function BacklogApp() {
   return (
